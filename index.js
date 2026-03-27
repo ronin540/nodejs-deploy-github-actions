59d3c9e378c494ae206e7b498f5425590685e27d
satyam@shopley <satyam@shopley.com>
2026-03-27 19:43:02 +0530

    added actions

--- index.js
+++ index.js
@@ -8,5 +8,5 @@ app.get('/', (req, res) => {
 })
 
 app.listen(PORT, () => {
-    console.log(`Server is running on ${PORT}`)
+    console.log(`Server is running on ${PORT} with git actions yayay`)
 } )
