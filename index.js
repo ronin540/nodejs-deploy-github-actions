5a2e312ea3248881dd4f6815bbf717ad9999faef
satyam@shopley <satyam@shopley.com>
2026-03-30 10:39:16 +0530

    testing api response

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express()
 const PORT = process.env.PORT || 8080
 
 app.get('/', (req, res) => {
-    return res.json({msg : "Hello from server"})
+    return res.json({msg : "Hello from server, actions are running"})
 })
 
 app.listen(PORT, () => {
